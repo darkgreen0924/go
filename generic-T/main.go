792891ec9cc8ebcbf7d1d86a69243b6d7ca9c7f5
leiqiqi4 <1429535947@qq.com>
2025-03-20 21:06:13 +0800

    interface

---
+++ generic-T/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	_case "godemo/generic-T/case"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	_case.Simple()
+	_case.TTypeCase()
+
+	// 不让主协程过早退出
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+	<-ctx.Done()
+}
