ee080931b46cda0dfc52b8541b28aaa6f1055f35
leiqiqi4 <1429535947@qq.com>
2025-03-22 19:53:59 +0800

    defer recover panic

--- defer-recover-panic/case/defer.go
+++ defer-recover-panic/case/defer.go
@@ -2,6 +2,30 @@ package _case
 
 import "fmt"
 
+// defer case
+func DeferCase() {
+	defer func() {
+		fmt.Println("defer defer")
+	}()
+	fmt.Println("函数执行")
+}
+
+// defer初始化值
+func DeferCase2() {
+	var i = 1
+
+	defer func(j int) {
+		fmt.Println("j:", j)
+	}(i + 1)
+
+	defer func() {
+		fmt.Println("i:", i+1)
+	}()
+	i = i + 99
+	fmt.Println("i:", i)
+
+}
+
 func ExceptionCase() {
 	defer func() {
 		err := recover()
--- defer-recover-panic/main.go
+++ defer-recover-panic/main.go
@@ -3,5 +3,7 @@ package main
 import _case "godemo/defer-recover-panic/case"
 
 func main() {
+	_case.DeferCase()
+	_case.DeferCase2()
 	_case.ExceptionCase()
 }
